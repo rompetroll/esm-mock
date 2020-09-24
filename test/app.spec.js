01c36f92fe56a42fe03091d23e5c52fb71d20ff3
Sven Eigenbrodt <sven@eigenbrodt.no>
2020-09-24 15:02:33 +0200

    try rewiremock

--- test/app.spec.js
+++ test/app.spec.js
@@ -2,11 +2,19 @@ import { doSomething } from '../lib/app.js';
 import tap from 'tap';
 import sinon from 'sinon';
 import * as db from '../lib/db.js'
+import 'rewiremock';
+
+const {plugins} = rewiremock;
+rewiremock.overrideEntryPoint(module);
+addPlugin(plugins.relative);
+addPlugin(plugins.usedByDefault);
+rewiremock.enable();
+const mockDB = {'value': 'Swankey'}
+rewiremock('../lib/db.js').with({
+  db: sinon.stub().resolves(mockDB)
+});
 
 tap.test('Should mock db value', (t) => {
-    const mockDB = {'value': 'Swankey'}
-    const dbstub = sinon.stub(db, 'loadDB').resolves(mockDB);
-
     doSomething().then(result => {
         console.log(result)
         t.equal(result, 'Hankey Swankey');
