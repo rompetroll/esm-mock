e9cc6994167ac5b3e82fbe0300d6f20c15957b9b
Sven Eigenbrodt <sven@eigenbrodt.no>
2020-09-24 19:07:35 +0200

    mock only in selected test-cases

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,13 +1,23 @@
 import { doSomething } from '../lib/app.js';
-import test from 'jest';
-import * as db from '../lib/db.js'
+import 'jest';
 
-const mockDB = {'value': 'Swankey'}
-jest.mock('../lib/db.js',() => ({
-    loadDB: name => new Promise((resolve, reject) => resolve(mockDB))
-}));
+beforeEach(() => {
+  jest.resetModules();
+});
+
+it('should work with original db', () => {
+    expect.assertions(1);
+    return expect(doSomething()).resolves.toEqual('Hankey Pankey');
+});
 
 it('should mock the DB', () => {
+    const mockDB = {'value': 'Swankey'}
+    jest.doMock('../lib/db.js',() => {
+        return {
+            __esModule: true,
+        loadDB : name => new Promise((resolve, reject) => resolve(mockDB))
+        }
+    });
     expect.assertions(1);
-    return expect(doSomething()).resolves.toEqual('Hankey Swankey');
+    return import('../lib/app.js').then(app => expect(app.doSomething()).resolves.toEqual('Hankey Swankey'));
 });
