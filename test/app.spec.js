c32ccb7caac0a69a5078e50802c959e6e26bf2b7
Sven Eigenbrodt <sven@eigenbrodt.no>
2020-09-24 18:17:48 +0200

    works with warnings

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,23 +1,13 @@
 import { doSomething } from '../lib/app.js';
-import tap from 'tap';
-import sinon from 'sinon';
+import test from 'jest';
 import * as db from '../lib/db.js'
-import 'rewiremock';
 
-const {plugins} = rewiremock;
-rewiremock.overrideEntryPoint(module);
-addPlugin(plugins.relative);
-addPlugin(plugins.usedByDefault);
-rewiremock.enable();
 const mockDB = {'value': 'Swankey'}
-rewiremock('../lib/db.js').with({
-  db: sinon.stub().resolves(mockDB)
-});
+jest.mock('../lib/db.js',() => ({
+    loadDB: name => new Promise((resolve, reject) => resolve(mockDB))
+}));
 
-tap.test('Should mock db value', (t) => {
-    doSomething().then(result => {
-        console.log(result)
-        t.equal(result, 'Hankey Swankey');
-        t.end();
-    }).catch(e => t.fail(e));
+it('should mock the DB', () => {
+    expect.assertions(1);
+    return expect(doSomething()).resolves.toEqual('Hankey Swankey');
 });
